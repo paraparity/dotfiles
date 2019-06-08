9b85ffc977be081203212820962ddefcf69b2155
paraparity <byoung1281@gmail.com>
2019-06-07 22:43:10 -0400

    Moved elisp files into configuration.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,34 @@
-;;; init.el --- paraparity's Emacs configuration file
-;;
+;;; init.el --- Load paraparity's literate configuration -*- lexical-binding: t -*-
 ;;; Commentary:
-;; Loads my literate configuration
-;;
+
+;; Bootstraps package and ensures use-package before loading my literate configuration.
+;; Contains a few initialization optimizations.
+
 ;;; Code:
+(setq debug-on-error t)
+
+;; I have often seen and considerd using gc-cons-threshold init optimizations, but haven't fully understood the why
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Garbage-Collection.html
+;; For now, lets just take a measurement to see if this warrants my attention
+;; Per: https://blog.d46.us/advanced-emacs-startup/
+(add-hook 'emacs-startup-hook
+		  (lambda ()
+			(message "Emacs ready in %s with %d garbage collections."
+					 (format "%.2f seconds"
+							 (float-time (time-subtract after-init-time before-init-time)))
+					 gcs-done)))
+
+;; Configure package.el to include melpa
+(require 'package)
+(setq package-archives '(("org"   . "https://orgmode.org/elpa/")
+						 ("gnu"   . "https://elpa.gnu.org/packages/")
+						 ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
-;(setq debug-on-error t)
+;; Ensure that use-package is installed.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
+;; Load from literate configuration
 (org-babel-load-file "~/.emacs.d/configuration.org")
