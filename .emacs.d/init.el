345e026cbb66641ba4871caf9830d95b8c91677d
paraparity <b.young1281@gmail.com>
2021-03-19 18:25:42 -0400

    Conditionally support native-comp; Add backwards compat for packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,53 +13,65 @@
 
 ;; Restore after startup
 (add-hook 'after-init-hook
-		  (lambda ()
-			(setq gc-cons-threshold 1000000)
-			(message "gc-cons-threshold restored to %s" gc-cons-threshold)))
+	  (lambda ()
+	    (setq gc-cons-threshold 1000000)
+	    (message "gc-cons-threshold restored to %s" gc-cons-threshold)))
 
 ;; Per: https://blog.d46.us/advanced-emacs-startup/
 ;; Let's monitor startup performance
 (add-hook 'emacs-startup-hook
-		  (lambda ()
-			(message "Emacs ready in %s with %d garbage collections."
-					 (format "%.2f seconds"
-							 (float-time (time-subtract after-init-time before-init-time)))
-					 gcs-done)))
+	  (lambda ()
+	    (message "Emacs ready in %s with %d garbage collections."
+		     (format "%.2f seconds"
+			     (float-time (time-subtract after-init-time before-init-time)))
+		     gcs-done)))
 
 (add-to-list 'load-path "~/.emacs.d/elpa")
 (add-to-list 'load-path "~/.emacs.d/elisp/")
 
 ;; Configure package.el to include melpa
-(require 'package)
-(setq package-archives '(("org"   . "https://orgmode.org/elpa/")
-						 ("gnu"   . "https://elpa.gnu.org/packages/")
-						 ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
-
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (setq package-archives '(("org"   . "https://orgmode.org/elpa/")
+			   ("gnu"   . "https://elpa.gnu.org/packages/")
+			   ("melpa" . "https://melpa.org/packages/")))
+  (package-initialize))
 
 ;; Ensure desired packages are installed, cache package refresh contents
 (unless	(and (package-installed-p 'use-package)
-			 (package-installed-p 'org-plus-contrib))
+	     (package-installed-p 'org-plus-contrib))
   (package-refresh-contents)
 
   (unless (package-installed-p 'use-package)
-  	(package-install 'use-package)
-  	(package-install 'use-package-ensure-system-package))
+    (package-install 'use-package)
+    (package-install 'use-package-ensure-system-package))
 
   (unless (package-installed-p 'org-plus-contrib)
-  	(package-install 'org-plus-contrib)))
+    (package-install 'org-plus-contrib)))
 
 (require 'use-package)
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
+;; Broken in [feature/native-comp]
+;; Ref:
+;; - https://github.com/dholm/benchmark-init-el/issues/15
+;; - https://github.com/hlissner/doom-emacs/issues/4534
+;;
 ;; Since we're benchmarking. Let's see where I can do better
-(use-package benchmark-init
-  :config
-  (add-hook 'after-init-hook 'benchmark-init/deactivate))
+;;(use-package benchmark-init
+;;  :ensure t
+;;  :config
+;;  (add-hook 'after-init-hook 'benchmark-init/deactivate))
+
+;; Conditionally enable native-comp
+(if (and (fboundp 'native-comp-available-p)
+	 (native-comp-available-p))
+    (setq comp-deferred-compilation t)
+  (message "Native compilation is *not* available"))
 
 (add-hook 'after-init-hook
-		  (lambda () (message "loaded in %s" (emacs-init-time))))
+	  (lambda () (message "loaded in %s" (emacs-init-time))))
 
 ;; Load from literate configuration
 (org-babel-load-file "~/.emacs.d/configuration.org")
