e9a330214f97c5e84d3e02ba82fe1679d960bfd1
paraparity <b.young1281@gmail.com>
2023-09-03 02:04:53 -0400

    Add module support and build flags to Emacs bootstrap

--- bootstraps/emacs-build-conf.sh
+++ bootstraps/emacs-build-conf.sh
@@ -4,9 +4,11 @@
 # Bootstrap commands for building Emacs from source
 ################################################################################
 git checkout emacs-29
+git pull
 
 ./autogen.sh
-./configure \
+CFLAGS="-ggdb3 -O0" CXXFLAGS="-ggdb3 -O0" LDFLAGS="-ggdb3"  ./configure \
     --with-native-compilation \
     --with-json \
-    --with-cairo
+    --with-cairo \
+    --with-modules
